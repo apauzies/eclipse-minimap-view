e28a6ccc01b79b0b2e5f9a7fb221deff30c3adf8
Alexandre Pauzies <alexandre@pauzies.com>
2012-09-29 03:53:34 -0400

    Highlight visible part of the editor

--- com.pauzies.minimap/src/com/pauzies/minimap/views/SampleView.java
+++ com.pauzies.minimap/src/com/pauzies/minimap/views/SampleView.java
@@ -1,16 +1,9 @@
 package com.pauzies.minimap.views;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
-import org.eclipse.jface.text.TextSelection;
-import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
-import org.eclipse.swt.events.PaintEvent;
-import org.eclipse.swt.events.PaintListener;
-import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
@@ -18,49 +11,52 @@ import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.editors.text.TextEditor;
 import org.eclipse.ui.part.ViewPart;
-import org.eclipse.ui.texteditor.AbstractTextEditor;
 
 public class SampleView extends ViewPart {
 
 	public static final String ID = "com.pauzies.minimap.views.SampleView";
-
-	TextEditor editor;
+	
+	private static final Color COLOR_DEFAULT = Display.getCurrent().getSystemColor(SWT.COLOR_WHITE);
+	private static final Color COLOR_HIGHLIGHT = new Color(Display.getCurrent(), 200,230,230);
+	
+	private static void fillMiniMap(StyledText minimap, TextEditor editor) {
+		StyledText text = editor.getMySourceViewer().getTextWidget();
+		minimap.setContent(text.getContent());
+	}
+	
+	private static void highlightVisibleRegion(StyledText minimap, TextEditor editor) {
+		int start = editor.getMySourceViewer().getTopIndexStartOffset();
+		int end = editor.getMySourceViewer().getBottomIndexEndOffset();
+		int startLine = minimap.getLineAtOffset(start);
+		int endLine = minimap.getLineAtOffset(end);
+		// Reset
+		minimap.setLineBackground(0, minimap.getLineCount() -1, COLOR_DEFAULT);
+		minimap.setLineBackground(startLine, endLine - startLine, COLOR_HIGHLIGHT);
+	}
 	
 	@Override
 	public void createPartControl(Composite parent) {
 
-		final StyledText text2 = new StyledText(parent, SWT.NONE);
-		text2.setEditable(false);
-		
+		final StyledText minimap = new StyledText(parent, SWT.NONE);
+		minimap.setEditable(false);
+		minimap.setEnabled(false);
+
 		Font font = new Font(parent.getDisplay(), "Arial", 4, SWT.NONE);
-		text2.setFont(font);
+		minimap.setFont(font);
 
-		getSite().getWorkbenchWindow().getSelectionService()
-				.addSelectionListener(new ISelectionListener() {
+		getSite().getWorkbenchWindow().getSelectionService().addSelectionListener(new ISelectionListener() {
 
-					@Override
-					public void selectionChanged(IWorkbenchPart part,
-							ISelection selection) {
-						System.err.println(part.toString() + selection);
-						if (part instanceof TextEditor) {
-							editor = (TextEditor) part;
-							StyledText text = editor.getMySourceViewer().getTextWidget();
-							text2.setContent(text.getContent());
-							if (selection instanceof TextSelection) {
-								TextSelection textSel = (TextSelection) selection;
-								int startLine = textSel.getStartLine();
-								int start = startLine > 20 ? startLine - 20 : 0;
-								int nbLine = 10;
-								text2.setLineBackground(start, nbLine, Display.getCurrent().getSystemColor(SWT.COLOR_YELLOW));
-							}
-							
-							text2.setLineBackground(0, 36, Display.getCurrent().getSystemColor(SWT.COLOR_YELLOW));
-//							text2.setSelectionRange(0, 800);
-//							text2.setBlockSelectionBounds(0, 0, 400, 400);
-						}
+			@Override
+			public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+				System.err.println(part.toString() + selection);
+				if (part instanceof TextEditor) {
+					TextEditor editor = (TextEditor) part;
+					fillMiniMap(minimap, editor);
+					highlightVisibleRegion(minimap, editor);
+				}
 
-					}
-				});
+			}
+		});
 
 		// getSourceViewer().getTextWidget().
 
