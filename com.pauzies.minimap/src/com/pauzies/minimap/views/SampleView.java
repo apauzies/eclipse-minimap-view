f476b566874925b604a3742c20562fabfe6e2a96
Alexandre Pauzies <alexandre@pauzies.com>
2013-01-10 23:11:50 -0500

    Use reflexion to bypass protected visibility => no need to modifier org.eclipse.ui.editors

--- com.pauzies.minimap/src/com/pauzies/minimap/views/SampleView.java
+++ com.pauzies.minimap/src/com/pauzies/minimap/views/SampleView.java
@@ -1,6 +1,10 @@
 package com.pauzies.minimap.views;
 
-import org.eclipse.jface.text.IViewportListener;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.eclipse.jface.text.source.ISourceViewer;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.BidiSegmentEvent;
@@ -8,8 +12,6 @@ import org.eclipse.swt.custom.BidiSegmentListener;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.ControlListener;
-import org.eclipse.swt.events.PaintEvent;
-import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Composite;
@@ -18,29 +20,71 @@ import org.eclipse.ui.ISelectionListener;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.editors.text.TextEditor;
 import org.eclipse.ui.part.ViewPart;
+import org.eclipse.ui.texteditor.AbstractTextEditor;
 
 public class SampleView extends ViewPart {
 
 	public static final String ID = "com.pauzies.minimap.views.SampleView";
-	
+
 	private static final Color COLOR_DEFAULT = Display.getCurrent().getSystemColor(SWT.COLOR_WHITE);
-	private static final Color COLOR_HIGHLIGHT = new Color(Display.getCurrent(), 200,230,230);
-	
-	private static void fillMiniMap(StyledText minimap, TextEditor editor) {
-		StyledText text = editor.getMySourceViewer().getTextWidget();
-		minimap.setContent(text.getContent());
+	private static final Color COLOR_HIGHLIGHT = new Color(Display.getCurrent(), 200, 230, 230);
+
+	class MyTextEditor {
+		private final AbstractTextEditor editor;
+
+		public MyTextEditor(AbstractTextEditor editor) {
+			this.editor = editor;
+		}
+
+		public ISourceViewer getSourceViewer() {
+			try {
+				//for (Method m : AbstractTextEditor.class.getDeclaredMethods()) {
+				//	System.err.println(m);
+				//}
+				//for (Field f : AbstractTextEditor.class.getDeclaredFields()) {
+				//	System.err.println(f);
+				//}
+				//Field f = AbstractTextEditor.class.getDeclaredField("fSourceViewer");
+				//f.setAccessible(true);
+				//return (ISourceViewer)f.get(editor);
+				Method m = AbstractTextEditor.class.getDeclaredMethod("getSourceViewer");
+				m.setAccessible(true);
+				return (ISourceViewer) m.invoke(editor);
+			} catch (SecurityException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InvocationTargetException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
+		}
 	}
 	
-	private static void highlightVisibleRegion(StyledText minimap, TextEditor editor) {
-		int start = editor.getMySourceViewer().getTopIndexStartOffset();
-		int end = editor.getMySourceViewer().getBottomIndexEndOffset();
+	private static void fillMiniMap(StyledText minimap, MyTextEditor editor) {
+		StyledText text = editor.getSourceViewer().getTextWidget();
+		minimap.setContent(text.getContent());
+	}
+
+	private static void highlightVisibleRegion(StyledText minimap, MyTextEditor editor) {
+		int start = editor.getSourceViewer().getTopIndexStartOffset();
+		int end = editor.getSourceViewer().getBottomIndexEndOffset();
 		int startLine = minimap.getLineAtOffset(start);
 		int endLine = minimap.getLineAtOffset(end);
 		// Reset
-		minimap.setLineBackground(0, minimap.getLineCount() -1, COLOR_DEFAULT);
+		minimap.setLineBackground(0, minimap.getLineCount() - 1, COLOR_DEFAULT);
 		minimap.setLineBackground(startLine, endLine - startLine, COLOR_HIGHLIGHT);
 	}
-	
+
 	@Override
 	public void createPartControl(Composite parent) {
 
@@ -56,26 +100,26 @@ public class SampleView extends ViewPart {
 			@Override
 			public void selectionChanged(IWorkbenchPart part, ISelection selection) {
 				System.err.println(part.toString() + selection);
-				if (part instanceof TextEditor) {
-					final TextEditor editor = (TextEditor) part;
+				if (part instanceof AbstractTextEditor) {
+					final MyTextEditor editor = new MyTextEditor((AbstractTextEditor) part);
 					fillMiniMap(minimap, editor);
 					highlightVisibleRegion(minimap, editor);
-					editor.getMySourceViewer().getTextWidget().addControlListener(new ControlListener() {
-						
+					editor.getSourceViewer().getTextWidget().addControlListener(new ControlListener() {
+
 						@Override
 						public void controlResized(ControlEvent e) {
 							// TODO Auto-generated method stub
 							highlightVisibleRegion(minimap, editor);
 						}
-						
+
 						@Override
 						public void controlMoved(ControlEvent e) {
 							// TODO Auto-generated method stub
-							
+
 						}
 					});
-					editor.getMySourceViewer().getTextWidget().addBidiSegmentListener(new BidiSegmentListener() {
-						
+					editor.getSourceViewer().getTextWidget().addBidiSegmentListener(new BidiSegmentListener() {
+
 						@Override
 						public void lineGetSegments(BidiSegmentEvent event) {
 							// TODO Auto-generated method stub
