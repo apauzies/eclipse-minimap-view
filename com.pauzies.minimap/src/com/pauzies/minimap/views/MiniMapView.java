ce7845fce18053dd523cc47a2e6b7fecde4c5e29
Alexandre Pauzies <alexandre@pauzies.com>
2013-01-11 23:49:04 -0500

    Can now click on the minimap to scroll in the main editor

--- com.pauzies.minimap/src/com/pauzies/minimap/views/MiniMapView.java
+++ com.pauzies.minimap/src/com/pauzies/minimap/views/MiniMapView.java
@@ -11,6 +11,10 @@ import org.eclipse.swt.custom.BidiSegmentListener;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.ControlListener;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Composite;
@@ -27,6 +31,12 @@ public class MiniMapView extends ViewPart {
 	private static final Color COLOR_DEFAULT = Display.getCurrent().getSystemColor(SWT.COLOR_LIST_BACKGROUND);
 	private static final Color COLOR_HIGHLIGHT = Display.getCurrent().getSystemColor(SWT.COLOR_LIST_SELECTION);
 	private static final Color COLOR_TEXT = Display.getCurrent().getSystemColor(SWT.COLOR_LIST_FOREGROUND);
+	private ISelectionListener selectionListener;
+	private ControlListener controlListener;
+	private BidiSegmentListener segmentListener;
+	private Font font = new Font(Display.getCurrent(), "Arial", 4, SWT.NONE);
+	private AbstractTextEditor selectedPart;
+	private MyTextEditor editor;
 
 	class MyTextEditor {
 		private final AbstractTextEditor editor;
@@ -59,7 +69,7 @@ public class MiniMapView extends ViewPart {
 			return null;
 		}
 	}
-	
+
 	private static void fillMiniMap(StyledText minimap, MyTextEditor editor) {
 		StyledText text = editor.getSourceViewer().getTextWidget();
 		minimap.setContent(text.getContent());
@@ -72,7 +82,7 @@ public class MiniMapView extends ViewPart {
 		int endLine = minimap.getLineAtOffset(end);
 		// Reset
 		minimap.setLineBackground(0, minimap.getLineCount() - 1, COLOR_DEFAULT);
-		minimap.setLineBackground(startLine, endLine - startLine, COLOR_HIGHLIGHT);
+		minimap.setLineBackground(startLine, endLine - startLine +1, COLOR_HIGHLIGHT);
 	}
 
 	@Override
@@ -80,51 +90,90 @@ public class MiniMapView extends ViewPart {
 
 		final StyledText minimap = new StyledText(parent, SWT.NONE);
 		minimap.setEditable(false);
-		minimap.setEnabled(false);
+		// minimap.set
+		minimap.setEnabled(true);
 		minimap.setForeground(COLOR_TEXT);
-		
-		Font font = new Font(parent.getDisplay(), "Arial", 4, SWT.NONE);
 		minimap.setFont(font);
-
-		getSite().getWorkbenchWindow().getSelectionService().addSelectionListener(new ISelectionListener() {
+		minimap.setCursor(null);
+		minimap.setCaret(null);
+		minimap.addMouseListener(new MouseAdapter() {
 
 			@Override
-			public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-				System.err.println(part.toString() + selection);
-				if (part instanceof AbstractTextEditor) {
-					final MyTextEditor editor = new MyTextEditor((AbstractTextEditor) part);
-					fillMiniMap(minimap, editor);
-					highlightVisibleRegion(minimap, editor);
-					editor.getSourceViewer().getTextWidget().addControlListener(new ControlListener() {
+			public void mouseDown(MouseEvent e) {
+				StyledText text = (StyledText) e.getSource();
+				int line = text.getLineAtOffset(text.getCaretOffset());
+				editor.getSourceViewer().getTextWidget().setCaretOffset(text.getCaretOffset());
+				editor.getSourceViewer().getTextWidget().setTopIndex(line);
+			}
 
-						@Override
-						public void controlResized(ControlEvent e) {
-							highlightVisibleRegion(minimap, editor);
-						}
-
-						@Override
-						public void controlMoved(ControlEvent e) {
-						}
-					});
-					editor.getSourceViewer().getTextWidget().addBidiSegmentListener(new BidiSegmentListener() {
-
-						@Override
-						public void lineGetSegments(BidiSegmentEvent event) {
-							highlightVisibleRegion(minimap, editor);
-						}
-					});
+		});
+		minimap.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				int offset = minimap.getCaretOffset();
+				if (offset == event.x) {
+					// right to left select
+					minimap.setSelection(event.x, event.x);
+				} else {
+					// left to right select
+					minimap.setSelection(event.y, event.y);
 				}
-
 			}
 		});
 
-		// getSourceViewer().getTextWidget().
+		controlListener = new ControlListener() {
 
+			@Override
+			public void controlResized(ControlEvent e) {
+				highlightVisibleRegion(minimap, editor);
+			}
+
+			@Override
+			public void controlMoved(ControlEvent e) {
+			}
+		};
+
+		segmentListener = new BidiSegmentListener() {
+
+			@Override
+			public void lineGetSegments(BidiSegmentEvent event) {
+				highlightVisibleRegion(minimap, editor);
+			}
+		};
+
+		selectionListener = new ISelectionListener() {
+
+			@Override
+			public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+				if (part == selectedPart) {
+					return;
+				}
+				if (!(part instanceof AbstractTextEditor)) {
+					return;
+				}
+				selectedPart = (AbstractTextEditor) part;
+				editor = new MyTextEditor((AbstractTextEditor) part);
+				fillMiniMap(minimap, editor);
+				highlightVisibleRegion(minimap, editor);
+				editor.getSourceViewer().getTextWidget().addControlListener(controlListener);
+				editor.getSourceViewer().getTextWidget().addBidiSegmentListener(segmentListener);
+			}
+
+		};
+
+		getSite().getWorkbenchWindow().getSelectionService().addSelectionListener(selectionListener);
 	}
 
 	@Override
 	public void setFocus() {
 
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+		editor.getSourceViewer().getTextWidget().removeControlListener(controlListener);
+		editor.getSourceViewer().getTextWidget().removeBidiSegmentListener(segmentListener);
+		getSite().getWorkbenchWindow().getSelectionService().removeSelectionListener(selectionListener);
 	}
 
 }
