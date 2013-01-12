65056f2f39fb78bf44fa9e4e8636fdb4ba5d2e34
Alexandre Pauzies <alexandre@pauzies.com>
2013-01-12 01:13:00 -0500

    Refactoring

--- com.pauzies.minimap/src/com/pauzies/minimap/views/MiniMapView.java
+++ com.pauzies.minimap/src/com/pauzies/minimap/views/MiniMapView.java
@@ -13,10 +13,12 @@ import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.ControlListener;
 import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.events.MouseMoveListener;
 import org.eclipse.swt.events.MouseWheelListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 import org.eclipse.swt.widgets.Composite;
@@ -30,17 +32,16 @@ public class MiniMapView extends ViewPart {
 
 	public static final String ID = "com.pauzies.minimap.views.MiniMapView";
 
-	private Color backgroundColor; // = Display.getCurrent().getSystemColor(SWT.COLOR_LIST_BACKGROUND);
-	private Color foregroundColor; // = Display.getCurrent().getSystemColor(SWT.COLOR_LIST_FOREGROUND);
-	private Color selectionBackgroundColor; // = Display.getCurrent().getSystemColor(SWT.COLOR_LIST_SELECTION);
-	private Color selectionForegroundColor; // = Display.getCurrent().getSystemColor(SWT.COLOR_LIST_SELECTION_TEXT);
+	private Color backgroundColor;
+	private Color foregroundColor;
+	private Color selectionBackgroundColor;
+	private Color selectionForegroundColor;
 
-	private ISelectionListener selectionListener;
-	private ControlListener controlListener;
-	private BidiSegmentListener segmentListener;
 	private Font font = new Font(Display.getCurrent(), "Arial", 4, SWT.NONE);
 	private AbstractTextEditor selectedPart;
 	private MyTextEditor editor;
+	private boolean mouseIsDown;
+	private StyledText minimap;
 
 	class MyTextEditor {
 		private final AbstractTextEditor editor;
@@ -86,127 +87,135 @@ public class MiniMapView extends ViewPart {
 		int endLine = minimap.getLineAtOffset(end);
 		// Reset
 		minimap.setLineBackground(0, minimap.getLineCount() - 1, backgroundColor);
-		minimap.setLineBackground(startLine, endLine - startLine +1, selectionBackgroundColor);
-		//minimap.setSelection(minimap.getOffsetAtLine(startLine), minimap.getOffsetAtLine(endLine - startLine +1));
+		minimap.setLineBackground(startLine, endLine - startLine + 1, selectionBackgroundColor);
+		// minimap.setSelection(minimap.getOffsetAtLine(startLine),
+		// minimap.getOffsetAtLine(endLine - startLine +1));
 	}
 
 	private void changeEditor(MouseEvent e) {
 		StyledText text = (StyledText) e.getSource();
 		int line = text.getLineAtOffset(text.getCaretOffset());
-		//editor.getSourceViewer().getTextWidget().setCaretOffset(text.getCaretOffset());
+		// editor.getSourceViewer().getTextWidget().setCaretOffset(text.getCaretOffset());
 		editor.getSourceViewer().getTextWidget().setTopIndex(line);
 	}
-	
-	boolean mouseIsDown;
-	
+
+	private final MouseListener minimapMouseListener = new MouseAdapter() {
+		@Override
+		public void mouseDown(MouseEvent e) {
+			changeEditor(e);
+			mouseIsDown = true;
+		}
+
+		@Override
+		public void mouseUp(MouseEvent e) {
+			changeEditor(e);
+			mouseIsDown = false;
+		}
+	};
+
+	private final MouseMoveListener minimapMouseMoveListener = new MouseMoveListener() {
+		@Override
+		public void mouseMove(MouseEvent e) {
+			if (mouseIsDown) {
+				changeEditor(e);
+			}
+		}
+	};
+
+	private final MouseWheelListener minimapMouseWheelListener = new MouseWheelListener() {
+		@Override
+		public void mouseScrolled(MouseEvent e) {
+			editor.getSourceViewer().getTextWidget().setTopIndex(editor.getSourceViewer().getTextWidget().getTopIndex() + (e.count * -1));
+		}
+	};
+
+	private final SelectionListener minimapSelectionListener = new SelectionAdapter() {
+		@Override
+		public void widgetSelected(SelectionEvent event) {
+			int offset = minimap.getCaretOffset();
+			if (offset == event.x) {
+				// right to left select
+				minimap.setSelection(event.x, event.x);
+			} else {
+				// left to right select
+				minimap.setSelection(event.y, event.y);
+			}
+		}
+	};
+
+	private final ControlListener editorControlListener = new ControlListener() {
+
+		@Override
+		public void controlResized(ControlEvent e) {
+			highlightVisibleRegion(minimap, editor);
+		}
+
+		@Override
+		public void controlMoved(ControlEvent e) {
+		}
+	};
+
+	private final ISelectionListener editorSelectionListener = new ISelectionListener() {
+
+		@Override
+		public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+			if (part == selectedPart) {
+				return;
+			}
+			if (!(part instanceof AbstractTextEditor)) {
+				return;
+			}
+			if (editor != null) {
+				editor.getSourceViewer().getTextWidget().removeControlListener(editorControlListener);
+				editor.getSourceViewer().getTextWidget().removeBidiSegmentListener(editorSegmentListener);
+			}
+
+			selectedPart = (AbstractTextEditor) part;
+			editor = new MyTextEditor((AbstractTextEditor) part);
+
+			fillMiniMap(minimap, editor);
+			highlightVisibleRegion(minimap, editor);
+
+			editor.getSourceViewer().getTextWidget().addControlListener(editorControlListener);
+			editor.getSourceViewer().getTextWidget().addBidiSegmentListener(editorSegmentListener);
+
+			foregroundColor = editor.getSourceViewer().getTextWidget().getForeground();
+			backgroundColor = editor.getSourceViewer().getTextWidget().getBackground();
+			selectionBackgroundColor = editor.getSourceViewer().getTextWidget().getSelectionBackground();
+			selectionForegroundColor = editor.getSourceViewer().getTextWidget().getSelectionForeground();
+
+			minimap.setBackground(backgroundColor);
+			minimap.setForeground(foregroundColor);
+			minimap.setSelectionBackground(selectionBackgroundColor);
+			minimap.setSelectionForeground(selectionForegroundColor);
+		}
+	};
+
+	private final BidiSegmentListener editorSegmentListener = new BidiSegmentListener() {
+
+		@Override
+		public void lineGetSegments(BidiSegmentEvent event) {
+			highlightVisibleRegion(minimap, editor);
+		}
+	};
+
 	@Override
 	public void createPartControl(Composite parent) {
 
-		final StyledText minimap = new StyledText(parent, SWT.NONE);
+		minimap = new StyledText(parent, SWT.NONE);
 		minimap.setEditable(false);
-		//minimap.setEnabled(true);
-
+		// minimap.setEnabled(true);
 		minimap.setFont(font);
-		//minimap.setCursor(null);
+		// minimap.setCursor(null);
 		minimap.setCaret(null);
-		
-		minimap.addMouseListener(new MouseAdapter() {
 
-			@Override
-			public void mouseDown(MouseEvent e) {
-				changeEditor(e);
-				mouseIsDown = true;
-			}
-			
-			@Override
-			public void mouseUp(MouseEvent e) {
-				changeEditor(e);
-				mouseIsDown = false;
-			}
-
-		});
-		minimap.addMouseMoveListener(new MouseMoveListener() {
-			
-			@Override
-			public void mouseMove(MouseEvent e) {
-				if (mouseIsDown) {
-					changeEditor(e);
-				}
-				
-			}
-		});
-		minimap.addMouseWheelListener(new MouseWheelListener() {
-			
-			@Override
-			public void mouseScrolled(MouseEvent e) {
-				editor.getSourceViewer().getTextWidget().setTopIndex(editor.getSourceViewer().getTextWidget().getTopIndex() + (e.count * -1));
-			}
-		});
+		minimap.addMouseListener(minimapMouseListener);
+		minimap.addMouseMoveListener(minimapMouseMoveListener);
+		minimap.addMouseWheelListener(minimapMouseWheelListener);
 		// Disable Selection
-		minimap.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent event) {
-				int offset = minimap.getCaretOffset();
-				if (offset == event.x) {
-					// right to left select
-					minimap.setSelection(event.x, event.x);
-				} else {
-					// left to right select
-					minimap.setSelection(event.y, event.y);
-				}
-			}
-		});
+		minimap.addSelectionListener(minimapSelectionListener);
 
-		controlListener = new ControlListener() {
-
-			@Override
-			public void controlResized(ControlEvent e) {
-				highlightVisibleRegion(minimap, editor);
-			}
-
-			@Override
-			public void controlMoved(ControlEvent e) {
-			}
-		};
-
-		segmentListener = new BidiSegmentListener() {
-
-			@Override
-			public void lineGetSegments(BidiSegmentEvent event) {
-				highlightVisibleRegion(minimap, editor);
-			}
-		};
-
-		selectionListener = new ISelectionListener() {
-
-			@Override
-			public void selectionChanged(IWorkbenchPart part, ISelection selection) {
-				if (part == selectedPart) {
-					return;
-				}
-				if (!(part instanceof AbstractTextEditor)) {
-					return;
-				}
-				selectedPart = (AbstractTextEditor) part;
-				editor = new MyTextEditor((AbstractTextEditor) part);
-				fillMiniMap(minimap, editor);
-				highlightVisibleRegion(minimap, editor);
-				editor.getSourceViewer().getTextWidget().addControlListener(controlListener);
-				editor.getSourceViewer().getTextWidget().addBidiSegmentListener(segmentListener);
-				
-				foregroundColor = editor.getSourceViewer().getTextWidget().getForeground();
-				backgroundColor = editor.getSourceViewer().getTextWidget().getBackground();
-				selectionBackgroundColor = editor.getSourceViewer().getTextWidget().getSelectionBackground();
-				selectionForegroundColor = editor.getSourceViewer().getTextWidget().getSelectionForeground();
-				
-				minimap.setBackground(backgroundColor);
-				minimap.setForeground(foregroundColor);
-				minimap.setSelectionBackground(selectionBackgroundColor);
-				minimap.setSelectionForeground(selectionForegroundColor);
-			}
-
-		};
-
-		getSite().getWorkbenchWindow().getSelectionService().addSelectionListener(selectionListener);
+		getSite().getWorkbenchWindow().getSelectionService().addSelectionListener(editorSelectionListener);
 	}
 
 	@Override
@@ -217,9 +226,9 @@ public class MiniMapView extends ViewPart {
 	@Override
 	public void dispose() {
 		super.dispose();
-		editor.getSourceViewer().getTextWidget().removeControlListener(controlListener);
-		editor.getSourceViewer().getTextWidget().removeBidiSegmentListener(segmentListener);
-		getSite().getWorkbenchWindow().getSelectionService().removeSelectionListener(selectionListener);
+		editor.getSourceViewer().getTextWidget().removeControlListener(editorControlListener);
+		editor.getSourceViewer().getTextWidget().removeBidiSegmentListener(editorSegmentListener);
+		getSite().getWorkbenchWindow().getSelectionService().removeSelectionListener(editorSelectionListener);
 	}
 
 }
